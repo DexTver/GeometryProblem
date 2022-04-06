ef5c417cce82c9867c0d05492ffe4d7f2442745c
dex_tver <ivan.sharapov.05@yandex.ru>
2022-04-06 19:52:27 +0300

    1

--- WideAngleClass.py
+++ WideAngleClass.py
@@ -4,3 +4,54 @@ class WideAngle:
     def __init__(self, x1, y1, x2, y2, x3, y3):
         # (x1, y1) и (x2, y2) задают отрезок; (x3, y3) задают сторону, в которую направлены прямые
         self.mainSegment = (x1, y1, x2, y2)
+        if x1 == x2:
+            if x3 - x1 >= 0:
+                x4 = x1 + 10000
+            else:
+                x4 = x1 - 10000
+            y4 = y1
+        elif y1 == y2:
+            if y3 - y1 >= 0:
+                y4 = y1 + 10000
+            else:
+                y4 = y1 - 10000
+            x4 = x1
+        else:
+            pos = ((x3 - x2) * (y1 - y2) - (y3 - y2) * (x1 - x2)) > 0
+            # переменная pos показывает по какую сторону от отрезка отложены лучи
+            k, b = create_equation(x1, y1, x2, y2)
+            # y = kx + b
+            b1 = x1 / k + y1
+            x4 = x1 + 1
+            y4 = -x4 / k + b1
+            line = ((x4 - x2) * (y1 - y2) - (y4 - y2) * (x1 - x2)) > 0
+            if pos == line:
+                if abs(y1 - y4) > 1:
+                    if y4 > y1:
+                        y4 = y1 + 10000
+                    else:
+                        y4 = y1 - 10000
+                    x4 = (b1 - y4) * k
+                else:
+                    x4 = x1 + 10000
+                    y4 = -x4 / k + b1
+            else:
+                if abs(y1 - y4) > 1:
+                    if y4 > y1:
+                        y4 = y1 - 10000
+                    else:
+                        y4 = y1 + 10000
+                    x4 = (b1 - y4) * k
+                else:
+                    x4 = x1 - 10000
+                    y4 = -x4 / k + b1
+        self.firstSegment = (x1, y1, x4, y4)
+        x5, y5 = x4 + x2 - x1, y4 + y2 - y1
+        self.secondSegment = (x2, y2, x5, y5)
+
+
+def create_equation(x1, y1, x2, y2):
+    # y = kx + b
+    k = (y2 - y1) / (x2 - x1)
+    b = y1 - x1 * (y2 - y1) / (x2 - x1)
+    return k, b
